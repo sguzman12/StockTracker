f5f3b83fb12f84d253c787c2bd6e2f827f7c1209
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-12 14:02:40 -0400

    Created a controller and controller folder for stockprices. Routing to api/stocks

---
+++ StockTrackerAPI/Controllers/StocksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockTrackerAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/stocks")]
+    public class StocksController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetStocks() 
+        {
+            return new JsonResult(StockDataStore.Current.Stocks);
+           
+        }
+    }
+}
