88c8511334059e0930502b81dae76a8b35252efe
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-13 17:07:42 -0400

    Refactored and included new attributes for the StockDto objects

--- StockTrackerAPI/StockDataStore.cs
+++ StockTrackerAPI/StockDataStore.cs
@@ -15,11 +15,11 @@ namespace StockTrackerAPI.Models
             // init dummy data
             Stocks = new List<StockDto>()
             {
-                new StockDto() { Id = 1, Name = "Google", Price = 122.45M},
-                new StockDto() { Id = 2, Name = "Apple", Price = 1066.65M},
-                new StockDto() { Id = 3, Name = "GE", Price = 45.22M},
-                new StockDto() { Id = 4, Name = "Toyota", Price = 1233.78M},
-                new StockDto() { Id = 5, Name = "Verizon", Price = 3662.34M},
+                new StockDto() { Id = 1, AlphaCode = "GOOG", Name = "Google", Price = 122.45M},
+                new StockDto() { Id = 2, AlphaCode = "APPE", Name = "Apple", Price = 1066.65M},
+                new StockDto() { Id = 3, AlphaCode = "GE",   Name = "GE", Price = 45.22M},
+                new StockDto() { Id = 4, AlphaCode = "TYTA",   Name = "Toyota", Price = 1233.78M},
+                new StockDto() { Id = 5, AlphaCode = "VZE",   Name = "Verizon", Price = 3662.34M},
             };
         }
     }
