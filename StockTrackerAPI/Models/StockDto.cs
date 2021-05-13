5936a52005e9e1469a15de5c5b72dfc8a3f008ee
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-13 17:07:03 -0400

    Added AlphaCode property to the model class

--- StockTrackerAPI/Models/StockDto.cs
+++ StockTrackerAPI/Models/StockDto.cs
@@ -8,6 +8,7 @@ namespace StockTrackerAPI.Models
     public class StockDto
     {
         public int Id { get; set; }
+        public string AlphaCode { get; set; }
         public string Name { get; set; }
         public  decimal Price { get; set; }
     }
