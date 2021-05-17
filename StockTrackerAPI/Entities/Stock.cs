7964ea7d8734fd6a553cbf37c3ef002dd1ec2840
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-17 18:54:58 -0400

    Created the Entity Frameworks class for Stocks

---
+++ StockTrackerAPI/Entities/Stock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Entities
+{
+    public class Stock
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid alpha code.")]
+        [MaxLength(6)]
+        public string AlphaCode { get; set; }
+
+        [Required(ErrorMessage = "Please provide a stock name.")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\$?\d+(\.(\d{2}))?$",
+           ErrorMessage = "Please provide value in the proper format (#.##)")]
+        public decimal Price { get; set; }
+    }
+}
