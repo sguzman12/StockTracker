20d16afeac2473803e5a6b92662d6e73cab2d237
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-17 18:54:14 -0400

    Created the DBContext class to use with Entity Framework and MySQL

---
+++ StockTrackerAPI/Contexts/StockInfoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockTrackerAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Contexts
+{
+    public class StockInfoContext : DbContext
+    {
+        public DbSet<Stock> Stocks { get; set; }
+
+        public StockInfoContext(DbContextOptions<StockInfoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
