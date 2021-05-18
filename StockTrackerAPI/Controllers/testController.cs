bc6eb4435b1d8645e7d7dd039d706674527837b1
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-18 18:39:36 -0400

    Created test controller for entity migration demoing

---
+++ StockTrackerAPI/Controllers/testController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockTrackerAPI.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/testdatabase")]
+    public class testController : ControllerBase
+    {
+        private readonly StockInfoContext stock;
+
+        public testController(StockInfoContext stock)
+        {
+            this.stock = stock;
+        }
+
+        public IActionResult TestDatabase() 
+        {
+            return Ok();
+        }
+    }
+}
