130586ea8eb6f2fd9a5bbe6717c30f2eaed29335
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-18 18:38:36 -0400

    Changed Spacing

--- StockTrackerAPI/Contexts/StockInfoContext.cs
+++ StockTrackerAPI/Contexts/StockInfoContext.cs
@@ -14,7 +14,7 @@ namespace StockTrackerAPI.Contexts
         public StockInfoContext(DbContextOptions<StockInfoContext> options)
             : base(options)
         {
-            Database.EnsureCreated();
+           Database.EnsureCreated();
         }
     }
 }
