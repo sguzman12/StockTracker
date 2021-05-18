3cffe716a4d3faaec39b7ad8db924c3f4a2d62ba
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-18 18:40:15 -0400

    Entity Framework created migrations of the stock database in MySQL

---
+++ StockTrackerAPI/Migrations/20210518221726_StockInfoDBInitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockTrackerAPI.Migrations
+{
+    public partial class StockInfoDBInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AlphaCode = table.Column<string>(maxLength: 6, nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    closingPrice = table.Column<decimal>(nullable: false),
+                    url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stocks");
+        }
+    }
+}
