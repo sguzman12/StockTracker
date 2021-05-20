b4eec8f45f88d031af24ea8f277671c3ed12365b
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-20 16:25:43 -0400

    Updated the migration to account for 2 Decimal places in the database for Price and Closing Price columns

---
+++ StockTrackerAPI/Migrations/20210520172423_SampleData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockTrackerAPI.Migrations
+{
+    public partial class SampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Stocks",
+                columns: new[] { "Id", "AlphaCode", "Name", "Price", "closingPrice", "url" },
+                values: new object[,]
+                {
+                    { 1, "FGZMX", "Guzman Inc", 136.41m, 178.24m, "https://www.nasdaq.com/market-activity/funds-and-etfs/fgzmx" },
+                    { 2, "GOOGL", "Google", 3343.31m, 2000.50m, "https://www.nasdaq.com/market-activity/stocks/goog" },
+                    { 3, "AMZN", "Amazon", 287.81m, 250.00m, "https://www.nasdaq.com/market-activity/stocks/amzn" },
+                    { 4, "WMT", "Walmart Inc", 14.81m, 14.60m, "https://www.nasdaq.com/market-activity/stocks/wmt" },
+                    { 5, "AMD", "Advanced Micro Devices, Inc.", 14.64m, 10.25m, "https://www.nasdaq.com/market-activity/stocks/amd" },
+                    { 6, "ZNGA", "Zynga Inc.", 10.70m, 8.62m, "https://www.nasdaq.com/market-activity/stocks/znga" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
---
+++ StockTrackerAPI/Migrations/20210520202340_UpdatedDecimalPlaces.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockTrackerAPI.Migrations
+{
+    public partial class UpdatedDecimalPlaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "closingPrice",
+                table: "Stocks",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Stocks",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "closingPrice",
+                table: "Stocks",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Stocks",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+        }
+    }
+}
