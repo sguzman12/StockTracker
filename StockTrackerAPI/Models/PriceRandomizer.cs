cc5b61a6b5b8bd44ab57dda6f0dfdebc7e994794
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-05-20 12:56:12 -0400

    Created price randomizer for later date to simulate prices dropping or rising

---
+++ StockTrackerAPI/Models/PriceRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Models
+{
+    public class PriceRandomizer
+    {
+        decimal MIN_VALUE = -3.0M;
+        decimal MAX_VALUE = 3.0M;
+
+        public decimal RandomizePrice(decimal price)
+        {
+            return price * (MAX_VALUE - MIN_VALUE) + MIN_VALUE;
+        }
+    }
+}
