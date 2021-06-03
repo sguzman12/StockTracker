f06c52dd65732fcec64de5affbb65b55bc57c5f0
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-06-03 10:24:52 -0400

    Created and Implemented a Repository for Stock Entity

---
+++ StockTrackerAPI/Services/IStockInfoRepository.cs
@@ -0,0 +1,20 @@
+﻿using StockTrackerAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Services
+{
+    /**
+     * Used for implementation of the Repository Pattern
+     * */
+    public interface IStockInfoRepository
+    {
+        //Can also be IQueryable. Depends on the type of API built. Pros and Cons for both.
+        IEnumerable<Stock> GetStocks();
+
+        Stock GetStock(int stockId);
+
+    }
+}
