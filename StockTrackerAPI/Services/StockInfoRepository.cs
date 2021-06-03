16fe12991a26ad9a3026c7eedc74749314f8009d
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-06-03 10:26:35 -0400

    Created and Implemented methods from the interface class.

---
+++ StockTrackerAPI/Services/StockInfoRepository.cs
@@ -0,0 +1,30 @@
+﻿using StockTrackerAPI.Contexts;
+using StockTrackerAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Services
+{
+    public class StockInfoRepository : IStockInfoRepository
+    {
+
+        private StockInfoContext context;
+
+        public StockInfoRepository(StockInfoContext context)
+        {
+            this.context = context;
+        }
+
+        public Stock GetStock(int stockId)
+        {
+            return context.Stocks.Where(s => s.Id == stockId).FirstOrDefault();
+        }
+
+        public IEnumerable<Stock> GetStocks()
+        {
+            return context.Stocks.OrderBy(s => s.Name).ToList();
+        }
+    }
+}
