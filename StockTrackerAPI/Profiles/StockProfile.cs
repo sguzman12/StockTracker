85d8f751f83abaf76944bfa765fab0363b6911aa
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-06-04 18:27:52 -0400

    Created a Profile folder and StockProfile class to handle AutoMapping

---
+++ StockTrackerAPI/Profiles/StockProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrackerAPI.Profiles
+{
+    /**
+     * Automapper Profile class for Stock Entity
+     * */
+    public class StockProfile : Profile
+    {
+        public StockProfile()
+        {
+            CreateMap<Entities.Stock, Models.StockDto>();
+        }
+
+
+    }
+}
